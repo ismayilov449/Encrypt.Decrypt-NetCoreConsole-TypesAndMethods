4ea7780f9159f58c637e0ceb1203c1b50808250b
Rüfət İsmayılov <rufet.ismayilov.1999@outlook.com>
2020-03-16 15:55:01 +0400

    SomeChanges

--- Encrypt-Decrypt.Algorithms/Algorithms/Keys/RSAKey.cs
+++ Encrypt-Decrypt.Algorithms/Algorithms/Keys/RSAKey.cs
@@ -50,24 +50,13 @@ namespace Encrypt_Decrypt.Algorithms.Algorithms.Keys
 
     public class RSAKey
     {
-        /// <summary>
-        /// Rsa public key
-        /// </summary>
+     
         public string PublicKey { get; set; }
-
-        /// <summary>
-        /// Rsa private key
-        /// </summary>
+ 
         public string PrivateKey { get; set; }
-
-        /// <summary>
-        /// Rsa public key Exponent
-        /// </summary>
+         
         public string Exponent { get; set; }
-
-        /// <summary>
-        /// Rsa public key Modulus
-        /// </summary>
+ 
         public string Modulus { get; set; }
     }
 }
