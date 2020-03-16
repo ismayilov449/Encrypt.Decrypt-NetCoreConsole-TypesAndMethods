a6bdb031238e60933ca8e015192d7720306be509
Rüfət İsmayılov <rufet.ismayilov.1999@outlook.com>
2020-03-16 15:50:12 +0400

    Add-RSA.Enc-Dec-Sign-Verify-Key

---
+++ Encrypt-Decrypt.Algorithms/Algorithms/Rsa/Extensions/RSASize.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encrypt_Decrypt.Algorithms.Algorithms.RSA.Extensions
+{
+    public enum RsaSize
+    {
+        R2048 = 2048,
+        R3072 = 3072,
+        R4096 = 4096
+    }
+}
