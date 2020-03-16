11fad5884c973b0850e1d686bc628436efb99cef
Rüfət İsmayılov <rufet.ismayilov.1999@outlook.com>
2020-03-16 13:56:50 +0400

    Add-DES.Enc-Dec-Key

---
+++ Encrypt-Decrypt.Algorithms/Algorithms/Keys/DESKey.cs
@@ -0,0 +1,20 @@
+﻿using Encrypt_Decrypt.Algorithms.Shared;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encrypt_Decrypt.Algorithms.Algorithms.Keys
+{
+    public class DESKey
+    {
+        public static string CreateDesKey()
+        {
+            return RandomStringGenerator.GetRandomStr(24);
+        }
+ 
+        public static string CreateDesIv()
+        {
+            return RandomStringGenerator.GetRandomStr(8);
+        }
+    }
+}
