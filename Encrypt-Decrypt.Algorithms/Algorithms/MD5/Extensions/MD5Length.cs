ccee9e4b44bcdecbc545e470b8430a0ddc00bca2
Rüfət İsmayılov <rufet.ismayilov.1999@outlook.com>
2020-03-16 16:53:47 +0400

    Add-MD5.Hasher

---
+++ Encrypt-Decrypt.Algorithms/Algorithms/MD5/Extensions/MD5Length.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encrypt_Decrypt.Algorithms.Algorithms.MD5.Extensions
+{
+    public enum MD5Length
+    {
+        L16 = 16,
+        L32 = 32
+    }
+}
---
+++ Encrypt-Decrypt.Algorithms/Algorithms/MD5/MD5Hasher.cs
@@ -0,0 +1,30 @@
+﻿using Encrypt_Decrypt.Algorithms.Algorithms.MD5.Extensions;
+using Encrypt_Decrypt.Algorithms.Shared;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encrypt_Decrypt.Algorithms.Algorithms.MD5
+{
+    public class MD5Hasher
+    {
+        public static string Md5(string srcString, MD5Length length = MD5Length.L32)
+        {
+            Check.Argument.IsNotEmpty(srcString, nameof(srcString));
+
+            string str_md5_out = string.Empty;
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] bytes_md5_in = Encoding.UTF8.GetBytes(srcString);
+                byte[] bytes_md5_out = md5.ComputeHash(bytes_md5_in);
+
+                str_md5_out = length == MD5Length.L32
+                    ? BitConverter.ToString(bytes_md5_out)
+                    : BitConverter.ToString(bytes_md5_out, 4, 8);
+
+                str_md5_out = str_md5_out.Replace("-", "");
+                return str_md5_out;
+            }
+        }
+    }
+}
